ef74ab6544906171d52c30bf5627c63eddc09b58
Niharikaparsi103 <niharika.parsi@peopletech.com>
2022-10-03 20:58:00 +0530

    shop

---
+++ src/com/shopping/ListOfSections.java
@@ -0,0 +1,639 @@
+package com.shopping;
+
+import java.util.Scanner;
+
+public class ListOfSections {
+	public static double sum;
+	
+	
+	public static int mobile()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 mobiles");
+		 System.out.println("enter number of mobiles :");
+		 int number = sc.nextInt();
+		 int m =number*15000;
+		 sum+=m;
+         System.out.println(m);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return m;
+	}
+	
+	public static int laptop()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 laptops");
+		 System.out.println("enter number of laptop :");
+		 int number = sc.nextInt();
+		 int n =number*40000;
+		 sum+=n;
+         System.out.println(n);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return n;
+	}
+ 
+	public static int headset()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 headset");
+		 System.out.println("enter number of headset :");
+		 int number = sc.nextInt();
+		 int o =number*40000;
+		 sum+=o;
+         System.out.println(o);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return o;
+	}
+	
+	
+	public static int smartwatch()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 smartwatch");
+		 System.out.println("enter number of smartwatch :");
+		 int number = sc.nextInt();
+		 int p =number*40000;
+		 sum+=p;
+         System.out.println(p);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return p;
+	}
+	
+	public static int speaker()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 speaker");
+		 System.out.println("enter number of speaker :");
+		 int number = sc.nextInt();
+		 int q =number*40000;
+		 sum+=q;
+         System.out.println(q);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return q;
+	}
+	
+	
+	public static int stove()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 stove");
+		 System.out.println("enter number of stove :");
+		 int number = sc.nextInt();
+		 int m =number*10000;
+		 sum+=m;
+         System.out.println(m);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return m;
+	}
+	
+	public static int oven()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 oven");
+		 System.out.println("enter number of oven :");
+		 int number = sc.nextInt();
+		 int n =number*5500;
+		 sum+=n;
+         System.out.println(n);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return n;
+	}
+ 
+	public static int grinder()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 grinder");
+		 System.out.println("enter number of grinder :");
+		 int number = sc.nextInt();
+		 int o =number*4300;
+		 sum+=o;
+         System.out.println(o);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return o;
+	}
+	
+	public static int dishes()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 dishes ");
+		 System.out.println("enter number of dishes :");
+		 int number = sc.nextInt();
+		 int p =number*1250;
+		 sum+=p;
+         System.out.println(p);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return p;
+	}
+	
+	public static int floormat()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 floormat");
+		 System.out.println("enter number of floormat :");
+		 int number = sc.nextInt();
+		 int q =number*440;
+		 sum+=q;
+         System.out.println(q);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return q;
+	}
+	
+	
+	public static int shampoo()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 shampoo");
+		 System.out.println("enter number of shampoo :");
+		 int number = sc.nextInt();
+		 int m =number*220;
+		 sum+=m;
+         System.out.println(m);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return m;
+	}
+	
+	public static int soap()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 soap");
+		 System.out.println("enter number of soap :");
+		 int number = sc.nextInt();
+		 int n =number*40;
+		 sum+=n;
+         System.out.println(n);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return n;
+	}
+ 
+	public static int bodywash()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 bodywash");
+		 System.out.println("enter number of bodywash:");
+		 int number = sc.nextInt();
+		 int o =number*450;
+		 sum+=o;
+         System.out.println(o);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return o;
+	}
+	
+	
+	public static int scrub()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 scrub");
+		 System.out.println("enter number of scrub :");
+		 int number = sc.nextInt();
+		 int p =number*15;
+		 sum+=p;
+         System.out.println(p);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return p;
+	}
+	
+
+	public static int partywear()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 partywear");
+		 System.out.println("enter number of partywear :");
+		 int number = sc.nextInt();
+		 int m =number*1500;
+		 sum+=m;
+         System.out.println(m);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return m;
+	}
+	
+	public static int causalwear()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 casualwear");
+		 System.out.println("enter number of casualwear :");
+		 int number = sc.nextInt();
+		 int n =number*400;
+		 sum+=n;
+         System.out.println(n);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return n;
+	}
+ 
+	public static int trousers()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 trouser");
+		 System.out.println("enter number of trouser :");
+		 int number = sc.nextInt();
+		 int o =number*1285;
+		 sum+=o;
+         System.out.println(o);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return o;
+	}
+	
+	
+	public static int jeans()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 jeans");
+		 System.out.println("enter number of jeans :");
+		 int number = sc.nextInt();
+		 int p =number*2500;
+		 sum+=p;
+         System.out.println(p);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return p;
+	}
+	
+	public static int westrenwear()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 westrenwear");
+		 System.out.println("enter number of westrenwear :");
+		 int number = sc.nextInt();
+		 int q =number*880;
+		 sum+=q;
+         System.out.println(q);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return q;
+	}
+	
+
+	public static int accesories()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 accesories");
+		 System.out.println("enter number of accesories :");
+		 int number = sc.nextInt();
+		 int m =number*1230;
+		 sum+=m;
+         System.out.println(m);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return m;
+	}
+	
+	public static int makeup()
+	{
+		
+		 Scanner sc= new Scanner(System.in);
+		 float before =100;
+		 float after;
+		 float quantity;
+		 System.out.println("avaliable 100 makeup");
+		 System.out.println("enter number of makeup :");
+		 int number = sc.nextInt();
+		 int n =number*3300;
+		 sum+=n;
+         System.out.println(n);
+         after=number;
+		 quantity = before-after;
+			System.out.println(quantity+"left quantity");
+			return n;
+	}
+ 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("-----WELCOME TO THE STORE-----");
+		System.out.println("==================================");
+		System.out.println("1-ELECTRONICS");
+		System.out.println("2-KITCHEN");
+		System.out.println("3-BATH");
+		System.out.println("4-CLOTHS");
+		System.out.println("5-OTHERS");
+		System.out.println("===================================");
+		
+		while(true) {
+		System.out.println(" please enter your choice");
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();
+		
+	    switch(choice) {
+	    case 1:
+	    	System.out.println("===SELECTED CHOICE IS ELECTRONIC===");
+	    	
+	    	System.out.println("-------------------------");
+	    	System.out.println("111---MOBILES------15000");
+	    	System.out.println("222---LAPTOPS------40000");
+	    	System.out.println("333---HEADSETS-----3000");
+	    	System.out.println("444---SMART WATCHES------2500");
+	    	System.out.println("555---SPEAKERS----7500");
+	    	System.out.println("-------------------------");
+	    	
+	    	System.out.println("selected choice item");
+	    	Scanner sc1 = new Scanner(System.in);
+	    	int item1 = sc.nextInt();
+	    	
+	    	if(item1==111) {
+	    		
+	    		mobile();
+	    	    }
+	    	
+	    		if(item1==222) {
+	    			
+	    		laptop();
+	    		}
+	    	
+	    		if(item1==333) {
+	    			
+	            headset();
+	    		}
+
+	    		if(item1==444) {
+	    			
+	            smartwatch();
+	    		}
+
+	    		if(item1==555) {
+	    			
+	                speaker();
+	    		}
+	           
+	    		else
+	    			System.out.println("entered code is invalid");
+         
+	    	break;
+	    case 2:
+	    	System.out.println("===SELECTED CHOICE IS KITCHEN===");
+	    	
+	    	System.out.println("-------------------------");
+	    	System.out.println("111---STOVE------10000");
+	    	System.out.println("222---OVEN------5500");
+	    	System.out.println("333---GRINDER-----4300");
+	    	System.out.println("444---DISHES------1250");
+	    	System.out.println("555---FLOOR MAT----440");
+	    	System.out.println("-------------------------");
+	    	
+	    	System.out.println("selected choice item");
+	    	Scanner sc2 = new Scanner(System.in);
+	    	int item2 = sc.nextInt();
+	    	
+               if(item2==111) {
+	    		
+	    		stove();
+	    	    }
+	    	
+	    		if(item2==222) {
+	    			
+	    		oven();
+	    		}
+	    	
+	    		if(item2==333) {
+	    			
+	            grinder();
+	    		}
+
+	    		if(item2==444) {
+	    			
+	            dishes();
+	    		}
+
+	    		if(item2==555) {
+	    			
+	                floormat();
+	    		}
+	           
+	    		else
+	    			System.out.println("entered code is invalid");
+         
+	    	break;
+        case 3:
+	    	System.out.println("===SELECTED CHOICE IS BATH===");
+	    	
+	    	System.out.println("-------------------------");
+	    	System.out.println("111---SHAMPOO--------220");
+	    	System.out.println("222---SOAPS--------40");
+	    	System.out.println("333---BODY WASH-------450");
+	    	System.out.println("444---SCURB &  BRUSHES-------15");
+	    	System.out.println("-------------------------");
+	    	
+	    	System.out.println("selected choice item");
+	    	Scanner sc3 = new Scanner(System.in);
+	    	int item3 = sc.nextInt();
+	    	
+	    	 if(item3==111) {
+		    		
+		    		shampoo();
+		    	    }
+		    	
+		    		if(item3==222) {
+		    			
+		    		soap();
+		    		}
+		    	
+		    		if(item3==333) {
+		    			
+		           bodywash();
+		    		}
+
+		    		if(item3==444) {
+		    			
+		            scrub();
+		    		}
+		           
+		    		else
+		    			System.out.println("entered code is invalid");
+	         
+	    	break;
+        case 4:
+	    	System.out.println("===SELECTED CHOICE IS CLOTHS===");
+	    	
+	    	System.out.println("-------------------------");
+	    	System.out.println("111---PARTY WEAR------1500");
+	    	System.out.println("222---CAUSAL WEAR------400");
+	    	System.out.println("333---TROUSERS------1285");
+	    	System.out.println("444---JEANS-------2500");
+	    	System.out.println("555---WEASTERN WEAR------880");
+	    	System.out.println("-------------------------");
+	    	
+	    	System.out.println("selected choice item");
+	    	Scanner sc4 = new Scanner(System.in);
+	    	int item4 = sc.nextInt();
+	    	
+                if(item4==111) {
+	    		
+	    		partywear();
+	    	    }
+	    	
+	    		if(item4==222) {
+	    			
+	    		causalwear();
+	    		}
+	    	
+	    		if(item4==333) {
+	    			
+	            trousers();
+	    		}
+
+	    		if(item4==444) {
+	    			
+	            jeans();
+	    		}
+
+	    		if(item4==555) {
+	    			
+	    			westrenwear();
+	    		}
+	    	break;	
+        case 5:
+        	System.out.println("===SELECTED CHOICE IS OTHERS===");
+        	
+        	System.out.println("-------------------------");
+	    	System.out.println("111---ACCESORIES-----1230");
+	    	System.out.println("222---MAKEUP PRODUCTS-----3300");
+	    	System.out.println("-------------------------");
+	    	
+	    	System.out.println("selected choice item");
+	    	Scanner sc5 = new Scanner(System.in);
+	    	int item5 = sc.nextInt();
+	    	
+             if(item5==111) {
+	    		
+	    		accesories();
+	    	    }
+	    	
+	    		if(item5==222) {
+	    			
+	    		makeup();
+	    		}
+        	break;
+        default:
+        	System.out.println("invalid value");
+	    }
+	    
+	    System.out.println("enter 1 to continue or any key to break");
+		int con=sc.nextInt();
+		   if(con==1){
+			   continue;
+			   }
+	      
+	       else{
+	    	  
+	           System.out.println("thank you and visit again");
+	           break;}
+		
+		}
+		System.out.println(sum+ "total amount");
+	}
+}
